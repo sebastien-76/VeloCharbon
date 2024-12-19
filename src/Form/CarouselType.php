8afe281d827000a6fee403c109003fe1ce9ded95
MrLkuch <kucheida.lucas@gmail.com>
2024-12-19 15:51:20 +0100

    feat(): Creation of carousel in Home page

---
+++ src/Form/CarouselType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Carousel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+
+class CarouselType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageFile', VichFileType::class, [
+                'required' => false,
+            ])
+           
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Carousel::class,
+        ]);
+    }
+}
